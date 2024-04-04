f4ea6b21abab1dc81c1915d96d0c777df0bb6d24
Juliia Sviderska <swiderskya111@gmail.com>
2024-04-04 18:33:53 +0200

    added new checks for array and palindrom

--- tests/practise.spec.ts
+++ tests/practise.spec.ts
@@ -1,5 +1,17 @@
 import {test, expect} from "@playwright/test"
 
+test('check  integer array', async() => {
+    function numArray(num) {
+        let set = new Set(num)
+        return set.size !== num.length
+    }
+    let num1 = [1, 2, 3, 5, 2]
+    let num2 = [1, 2, 3, 4, 5, 6]
+    
+    console.log(numArray(num1))  //true
+    console.log(numArray(num2))  //false
+})
+
 test('check palindrome string', async() => {
     function isPalindrome(string) {
         string = string.toLowerCase().replace(/[^A-Z0-9]/ig, "")
